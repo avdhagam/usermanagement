69616c6048f252d8c59d412611859a2e09efc973
avdhagam <davanisri@gmail.com>
2025-01-26 16:08:31 +0530

    added UsersRepository interface

--- src/main/java/com/example/ui/shared/dto/UserDto.java
+++ src/main/java/com/example/ui/shared/dto/UserDto.java
@@ -19,5 +19,5 @@ public class UserDto implements Serializable {
     private String password;
     private String encryptedPassword;
     private String emailVerificationToken;
-    private String getEmailVerificationStatus;
+    private Boolean getEmailVerificationStatus = false;
 }
